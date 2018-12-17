b513b9ead607732fa4d39c5c965365c255626c77
関根健太 <jr.pkikansha.sr@gmail.com>
2018-12-18 00:12:03 +0900

    CommandListのファイル関連処理を追加

--- MediaConverter/CommandList.cs
+++ MediaConverter/CommandList.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.IO;
+using System.Windows.Forms;
 
 /*
  * コマンド実行を管理
@@ -14,35 +15,102 @@ namespace MediaConverter
 {
     class CommandList
     {
-        //コマンドの基本的な情報を格納するクラス
-        private class CommandBaseData
-        {
-            public String CommandBaseExpression;    //基本的なコマンド
-        }
 
-        public List<String> Commands;               //実行するコマンドの元の文字列
+        public String Commands;               //実行するコマンドの元の文字列
 
         //必要な置換をする正規表現リスト
         public List<Tuple<String, String>> ReplaceRegulExpress;
 
+
         public CommandList()
         {
-            Commands = new List<string>();
-            Commands.Add("sleep");
+            //Commands = new List<string>();
+            //Commands.Add("sleep");
 
             ReplaceRegulExpress = new List<Tuple<string, string>>();
+
+            //デフォルトで置換する表現のリストをあらかじめ入れておく
+            ReplaceRegulExpress.Add(new Tuple<string, string>("<%InputFile%>", "None"));
+            ReplaceRegulExpress.Add(new Tuple<string, string>("<%OutputFolder%>", "None"));
+            ReplaceRegulExpress.Add(new Tuple<string, string>("<%OutputFile%>", "Out"));
+        }
+
+        //入力ファイルのパスをセットする
+        public void SetInputFileName(MediaFiles mf)
+        {
+            if (!File.Exists(mf.Name))
+            {
+                //存在しないファイルが指定されたら例外を投げる
+                throw new FileNotFoundException();
+            }
+
+            int Index = (int)ReplaceRegulExpress.FindIndex(Dat => Dat.Item1 == "<%InputFile%>");
+            if(Index >= 0)
+            {
+                ReplaceRegulExpress.RemoveAt(Index);
+            }
+
+            ReplaceRegulExpress.Add(new Tuple<String, String>("<%InputFile%>", mf.Name));
+
+            return;
+        }
+
+        //出力ファイルのファイル名をセットする
+        public void SetOutputFileName(String OutFileName)
+        {
+            int Index = (int)ReplaceRegulExpress.FindIndex(Dat => Dat.Item1 == "<%OutputFile%>");
+            if (Index >= 0)
+            {
+                ReplaceRegulExpress.RemoveAt(Index);
+            }
+            ReplaceRegulExpress.Add(new Tuple<String, String>("<%OutputFile%>", OutFileName));
+
+            return;
+        }
+
+        //出力するフォルダ名をセットする
+        public void SetOutputFolder(String FolderPath)
+        {
+            if (!File.Exists(FolderPath))
+            {
+                //再帰的にディレクトリを作成する
+                try
+                {
+                    Directory.CreateDirectory(FolderPath);
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e);
+
+                    //エラーダイアログを出す
+                    //MessageBox.Show("間違ったパス指定です\n" + e.ToString(), "エラー",
+                    //    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //return;
+
+                    //または例外を投げる
+                    throw e;
+                }
+            }
+
+            int Index = (int)ReplaceRegulExpress.FindIndex(Dat => Dat.Item1 == "<%OutputFolder%>");
+            if (Index >= 0)
+            {
+                ReplaceRegulExpress.RemoveAt(Index);
+            }
+            ReplaceRegulExpress.Add(new Tuple<String, String>("<%OutputFolder%>", FolderPath));
+
+            return;
         }
 
         //置換済みのコマンドを返す
         public String ReplacedCommand(int CommandIndex)
         {
-            if(CommandIndex < Commands.Count)
-            {
-                return "";
-            }
+            String OutCommand = Commands;
 
-            String OutCommand = Commands[CommandIndex];
+            //ファイルに関する置換
 
+
+            //その他の置換
             foreach(Tuple<String, String> str in ReplaceRegulExpress)
             {
                 OutCommand = Regex.Replace(OutCommand, str.Item1, str.Item2);
@@ -70,7 +138,8 @@ namespace MediaConverter
                     System.Diagnostics.Debug.WriteLine(line);
 
                     //MatchCollection matche = Regex.Matches(line, "\".*?\"");
-                    Match m = Regex.Match(line, "(?<rep>(\".*?\"|^\"*?)):(?<torep>\".*?\"|^\"*?)");
+                    //Match m = Regex.Match(line, "(?<rep>(\".*?\"|^\"*?)):(?<torep>\".*?\"|^\"*?)");
+                    Match m = Regex.Match(line, "(?<rep>.*):(?<torep>.*)");
                     System.Diagnostics.Debug.WriteLine(m.Groups["rep"].ToString());
                     System.Diagnostics.Debug.WriteLine(m.Groups["torep"].ToString());
                 }
